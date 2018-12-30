41bc394efae3e64ea21e4ee1464a66ae2260516d
Arne Maier <Arne_Maier@gmx.de>
2018-12-30 15:43:08 +0100

    Add add user and invited person

--- index.js
+++ index.js
@@ -13,11 +13,48 @@ function logUser() {
         .once('value')
         .then((respose) => {
             respose.forEach(user => console.log(user.val()))
-        });
+        })
+        .then(() => firebase.app().delete());
 }
 
 function addUser(name) {
-    console.log('Add user');
+    if (!name) {
+        return console.error('Second parameter is missing');
+    }
+    console.log(`Add User with internalName ${name}.`);
+    firebase.database().ref('/users')
+        .push({
+            interalName: name,
+            responded: false,
+            mailUpdate: '',
+        })
+        .then(() => {
+            console.log(`User added.`);
+            firebase.app().delete();
+
+        })
+        .catch(error => console.error(error))
+        .then(() => firebase.app().delete());
+}
+
+function addInvitedPerson(name, userId) {
+    if (!name) {
+        return console.error('Second parameter is missing');
+    }
+    if (!userId) {
+        return console.error('Third parameter is missing');
+    }
+    console.log(`Add invited person ${name} to user ${userId}`);
+    firebase.database().ref(`/users/${userId}`)
+        .push({
+            name: name,
+            participate: 'Yes',
+            food: 'Meat',
+            allergies: '',
+        })
+        .then(() => console.log(`Added invited Person ${name} to ${userId}.`))
+        .catch((error) => console.error(`Could not add ${name} because of an error. \n ${error}`))
+        .then(() => firebase.app().delete());
 }
 
 
@@ -34,20 +71,28 @@ function addPresent(name) {
             pictureUrl: '',
             reservedBy: '',
          })
-        .then(() => console.log(`${name} added to presents`));
+        .then(() => console.log(`${name} added to presents.`))
+        .then(() => firebase.app().delete());
 }
 
 const Actions = {
     showUsers: 'showusers',
     addUser: 'adduser',
     addPresent: 'addpresent',
+    addInvitedPerson: 'addinvitedperson',
 };
 
-const [ _, __, action, name ] = process.argv;
-console.log(`${action} called ${name ? `with ${name}` : null}`);
+const [ _, __, action, name, ref ] = process.argv;
+console.log(`${action} is called`);
 switch (action.toLowerCase()) {
     case Actions.showUsers:
         logUser();
+        break;
+    case Actions.addUser:
+        addUser(name);
+        break;
+    case Actions.addInvitedPerson:
+        addInvitedPerson(name, ref);
         break;
     case Actions.addPresent:
         addPresent(name);
